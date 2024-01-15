efff766753f3db1441945f1db5f00c21994e867d
Kai Yao <kai.yao@hyatt.com>
2024-01-15 17:31:44 -0600

    started header

---
+++ src/components/ResumeHeader.tsx
@@ -0,0 +1,8 @@
+import "./ResumeHeader.css";
+
+export const ResumeHeader: React.FC = () => (
+  <header>
+    <h1 className="name">Kai Yao</h1>
+    <p className="job-title">Senior Software Engineer</p>
+  </header>
+);

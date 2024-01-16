3d78468712429478967f850682df35be85a5b9f8
Kai Yao <kai.yao@hyatt.com>
2024-01-16 09:55:52 -0600

    Added contact info to resume header

--- src/components/ResumeHeader.tsx
+++ src/components/ResumeHeader.tsx
@@ -2,7 +2,17 @@ import "./ResumeHeader.css";
 
 export const ResumeHeader: React.FC = () => (
   <header>
-    <h1 className="name">Kai Yao</h1>
-    <p className="job-title">Senior Software Engineer</p>
+    <section className="header-title-area">
+      <h1 className="name">Kai Yao</h1>
+      <p className="job-title">Senior Software Engineer</p>
+    </section>
+    <section className="contact-info">
+      <p className="email">
+        <a href="mailto:kai.b.yao@gmail.com">kai.b.yao@gmail.com</a>
+      </p>
+      <p className="phone">
+        <a href="tel:630-788-0424">630.788.0424</a>
+      </p>
+    </section>
   </header>
 );

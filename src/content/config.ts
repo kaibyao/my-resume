820a618d29d0ef71d50a6b613f56b4c0aeaa4a40
Kai Yao <kai.yao@hyatt.com>
2024-01-20 13:51:41 -0600

    Added experience content

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content";
+
+const experienceCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    organization: z.string(),
+    startDate: z.date(),
+    endDate: z
+      .date()
+      .optional()
+      .describe("If omitted, assume this is your current position."),
+  }),
+});
+
+export const collections = {
+  experience: experienceCollection,
+};
